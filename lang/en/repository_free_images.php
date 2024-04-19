964a2ba84a178545aee86cef0f0f5135d3885089
David OC <davidherzlos@gmail.com>
2024-04-19 07:54:22 -0600

    first commit

---
+++ lang/en/repository_free_images.php
@@ -0,0 +1,35 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'repository_free_images', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   repository_free_images
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['keyword'] = 'Search for';
+$string['pluginname'] = 'Free images';
+$string['free_images:view'] = 'View free_images repository';
+$string['configplugin'] = 'Free images repository type configuration';
+$string['maxwidth'] = 'Max image width (px)';
+$string['maxheight'] = 'Max image height (px)';
+$string['privacy:metadata:repository_free_images'] = 'The Free images repository plugin does store user preferences, and transmits user data from Moodle to the remote system.';
+$string['privacy:metadata:repository_free_images:search_text'] = 'The Free images repository user search text query.';
+$string['privacy:metadata:repository_free_images:preference:maxwidth'] = 'The user preference max width configured for the Free images repository';
+$string['privacy:metadata:repository_free_images:preference:maxheight'] = 'The user preference Max Height configured for the Free images repository.';
